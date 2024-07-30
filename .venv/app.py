5c4d0a78cfbecebe088de5785a966b144e046c12
Parsa <parsamanes@yahoo.com>
2024-07-30 12:59:37 +0300

    wtforms

--- .venv/app.py
+++ .venv/app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'somepassword' #dont push it to github
+
+# create a class form :thumbsup
+class NamerForm(FlaskForm):
+    name = StringField('Whats your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 @app.route('/')
 def index():
@@ -25,6 +35,16 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
+
+@app.route('/name', methods = ['POST', 'GET'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('name.html', name = name, form = form)
 
 if __name__ == '__main__':
     app.run(debug=True)
